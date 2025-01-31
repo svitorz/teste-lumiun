abb23bf11f443e30ee5f657359f0e033b5df228e
svitorz <vitor.fabio.castro@gmail.com>
2025-01-31 19:08:53 -0300

    create the domain trait and the firsts views

---
+++ app/Traits/DomainTrait.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Traits;
+
+use App\Models\Domain;
+
+trait DomainiTrait {
+
+    public function save(Domain $domain){
+        //
+    }
+
+
+    public function validateFile($file)
+    {
+
+    }
+
+}
